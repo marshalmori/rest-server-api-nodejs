7f7bac7f3c2f49a1ad9f7cdbb371fcaeea0d07a9
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-11-01 08:53:33 -0300

    Parámetros de segmento y query

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,30 +1,43 @@
-const { response } = require("express");
+const { response, request } = require("express");
 
-const usuariosGet = (req, res) => {
+const usuariosGet = (req = request, res = response) => {
+  const { q, nombre = "No name", apikey, page = 1, limit = 5 } = req.query;
+
   res.json({
     msg: "GET API - controlador",
+    q,
+    nombre,
+    apikey,
+    page,
+    limit,
   });
 };
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = (req = request, res = response) => {
+  const { nombre, edad } = req.body;
   res.json({
     msg: "POST API - controlador",
+    nombre,
+    edad,
   });
 };
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = (req = request, res = response) => {
+  //   const id = req.params.id;
+  const { id } = req.params;
   res.json({
     msg: "PUT API - controlador",
+    id,
   });
 };
 
-const usuariosPatch = (req, res = response) => {
+const usuariosPatch = (req = request, res = response) => {
   res.json({
     msg: "PATCH API - controlador",
   });
 };
 
-const usuariosDelete = (req, res = response) => {
+const usuariosDelete = (req = request, res = response) => {
   res.json({
     msg: "DELETE API - controlador",
   });
