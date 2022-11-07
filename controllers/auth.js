60d74925c10d5ab63e6439b7b6cf9f689758eb09
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-11-07 16:47:00 -0300

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,46 @@
 const { response } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const login = (req, res = response) => {
-  res.json({
-    msg: "Login OK!",
-  });
+const Usuario = require("../models/usuario");
+
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // Verificar se o email existe no body
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password não estão corretos.",
+      });
+    }
+
+    // Verificar se o usuário está ativo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password não estão corretos.",
+      });
+    }
+
+    // Verificar a senha
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password não estão corretos. passwd",
+      });
+    }
+
+    // Gerar o JWT
+
+    res.json({
+      msg: "Login OK!",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Fale com o adminstrador do sistema.",
+    });
+  }
 };
 
 module.exports = {
