76d52b02c374d1d79f8a57e982ed17c33e8c8543
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-11-29 22:13:29 -0300

    Ruta para realizar búsquedas

---
+++ controllers/buscar.js
@@ -0,0 +1,13 @@
+const { response } = require("express");
+
+const buscar = (req, res = response) => {
+  const { coleccion, termino } = req.params;
+  res.json({
+    coleccion,
+    termino,
+  });
+};
+
+module.exports = {
+  buscar,
+};
