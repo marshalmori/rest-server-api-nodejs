522d0e738f8f4ded8b9148d540099ae32a162611
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-11-29 22:56:08 -0300

    Búsquedas en base de datos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,11 +1,43 @@
 const { response } = require("express");
+const { Usuario, Categoria, Producto } = require("../models");
+const { ObjectId } = require("mongoose").Types;
+
+const coleccionesPermitidas = ["usuarios", "categorias", "productos", "roles"];
+
+const buscarUsuarios = async (termino = "", res = response) => {
+  const esMongoID = ObjectId.isValid(termino);
+
+  if (esMongoID) {
+    const usuario = await Usuario.findById(termino);
+    res.json({
+      results: usuario ? [usuario] : [],
+    });
+  }
+};
 
 const buscar = (req, res = response) => {
   const { coleccion, termino } = req.params;
-  res.json({
-    coleccion,
-    termino,
-  });
+
+  if (!coleccionesPermitidas.includes(coleccion)) {
+    return res.status(400).json({
+      msg: `As coleções permitidas são: ${coleccionesPermitidas}`,
+    });
+  }
+
+  switch (coleccion) {
+    case "usuarios":
+      buscarUsuarios(termino, res);
+      break;
+    case "categorias":
+      break;
+    case "productos":
+      break;
+
+    default:
+      res.status(500).json({
+        msg: "Você esqueceu de fazer esta busca.",
+      });
+  }
 };
 
 module.exports = {
