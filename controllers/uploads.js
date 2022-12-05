28bd10f2f7559cd3bcffb8c2a27191ac4800e293
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-12-05 20:40:39 -0300

    Validar la extensión

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -14,16 +14,27 @@ const cargarArchivo = (req, res = response) => {
   }
 
   const { archivo } = req.files;
+  const nombreCortado = archivo.name.split(".");
+  const extension = nombreCortado[nombreCortado.length - 1];
 
-  const uploadPath = path.join(__dirname, "../uploads/", archivo.name);
+  // Validar a extensão
+  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
+  if (!extensionesValidas.includes(extension)) {
+    return res.status(400).json({
+      msg: `A extensão ${extension} não é permitida. As extensões permitidas são: ${extensionesValidas}`,
+    });
+  }
+  res.json({ extension });
 
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
+  // const uploadPath = path.join(__dirname, "../uploads/", archivo.name);
 
-    res.json({ msg: "File uploaded to " + uploadPath });
-  });
+  // archivo.mv(uploadPath, (err) => {
+  //   if (err) {
+  //     return res.status(500).json({ err });
+  //   }
+
+  //   res.json({ msg: "File uploaded to " + uploadPath });
+  // });
 };
 
 module.exports = {
