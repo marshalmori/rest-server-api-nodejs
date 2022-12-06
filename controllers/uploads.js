f2ce4eb79775e22ac61b3090249b44dcd6e7b70e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-12-06 17:23:13 -0300

    Crear carpetas de destino

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -3,15 +3,20 @@ const { subirArchivo } = require("../helpers/subir-archivo");
 
 const cargarArchivo = async (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-    res.status(400).json({ msg: "Não nenhum arquivo para upload" });
+    res.status(400).json({ msg: "Não há nenhum arquivo para upload" });
     return;
   }
 
-  const nombre = await subirArchivo(req.files);
+  try {
+    // const nombre = await subirArchivo(req.files, ["txt", "md"], "textos");
+    const nombre = await subirArchivo(req.files, undefined, "imgs");
 
-  res.json({
-    nombre,
-  });
+    res.json({
+      nombre,
+    });
+  } catch (msg) {
+    res.status(400).json({ msg });
+  }
 };
 
 module.exports = {
