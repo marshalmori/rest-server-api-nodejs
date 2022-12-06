8f5247d7eab5eac71292bbba3bba8c7a7864ded1
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-12-06 11:49:25 -0300

    Crear Helper - SubirArchivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,40 +1,16 @@
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
-
 const { response } = require("express");
+const { subirArchivo } = require("../helpers/subir-archivo");
 
-const cargarArchivo = (req, res = response) => {
-  if (!req.files || Object.keys(req.files).length === 0) {
+const cargarArchivo = async (req, res = response) => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({ msg: "Não nenhum arquivo para upload" });
     return;
   }
 
-  if (!req.files.archivo) {
-    res.status(400).json({ msg: "Não nenhum arquivo para upload" });
-    return;
-  }
+  const nombre = await subirArchivo(req.files);
 
-  const { archivo } = req.files;
-  const nombreCortado = archivo.name.split(".");
-  const extension = nombreCortado[nombreCortado.length - 1];
-
-  // Validar a extensão
-  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
-  if (!extensionesValidas.includes(extension)) {
-    return res.status(400).json({
-      msg: `A extensão ${extension} não é permitida. As extensões permitidas são: ${extensionesValidas}`,
-    });
-  }
-
-  const nombreTemp = uuidv4() + "." + extension;
-  const uploadPath = path.join(__dirname, "../uploads/", nombreTemp);
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-
-    res.json({ msg: "File uploaded to " + uploadPath });
+  res.json({
+    nombre,
   });
 };
 
