2f3e17151b9641862de7f0543ebb810777249dcf
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-12-07 16:11:21 -0300

    Borrar imágenes de Cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -151,6 +151,10 @@ const actualizarImagenCloudinary = async (req, res = response) => {
   // Deletar as imagens anteriores
   if (modelo.img) {
     // Tem que deletar a imagem da cloudinary
+    const nombreArr = modelo.img.split("/");
+    const nombre = nombreArr[nombreArr.length - 1];
+    const [public_id] = nombre.split(".");
+    cloudinary.uploader.destroy(public_id);
   }
 
   const { tempFilePath } = req.files.archivo;
